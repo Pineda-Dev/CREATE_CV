5c716f531ef8af785940f82db8627454a3c94da5
ELMER REYES <elmer.reyes.pineda@gmail.com>
2026-01-26 13:52:51 -0600

    Se eliminan características y se agrega nuevo modo para descargar PDF

--- app.js
+++ app.js
@@ -16,47 +16,12 @@ function update(field, value) {
   if (el) el.innerText = value;
 }
 
-function setTheme(theme) {
-  document.body.className = "theme-" + theme;
-}
-
-function setPremiumTemplate(template) {
-  const cv = document.getElementById("cv");
-
-  cv.classList.remove(
-    "template-modern",
-    "template-creative",
-    "template-executive"
-  );
-
-  cv.classList.add("template-" + template);
-}
-
-function downloadPDF() {
-  html2pdf().from(document.getElementById("cv")).save("cv.pdf");
-}
-
-/* FOTO DE PERFIL */
 function loadPhoto(event) {
   const file = event.target.files[0];
   if (!file) return;
-
   const reader = new FileReader();
   reader.onload = () => {
     document.getElementById("photoPreview").src = reader.result;
   };
   reader.readAsDataURL(file);
-}
-
-
-function setLayout(type) {
-  const cv = document.getElementById("cv");
-  cv.classList.remove("one-column", "two-column");
-  cv.classList.add(type + "-column");
-}
-
-function setLanguage(lang) {
-  document.querySelectorAll("[data-es]").forEach(el => {
-    el.innerText = el.dataset[lang];
-  });
 }
